cdf40d51cf7fc34a8f9b48af4f2fed3ccadb3db7
Kathan <kathanmajithia@gmail.com>
2026-08-09 22:27:20 +0530

    Sequence runnables

---
+++ Runnables/runnable_seq.py
@@ -0,0 +1,36 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_classic.output_parsers import StructuredOutputParser, ResponseSchema, PydanticOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from langchain_classic.schema.runnable import RunnableSequence
+
+from pydantic import BaseModel, Field
+from typing import Literal
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="Qwen/Qwen2.5-7B-Instruct",task="text-generation",max_new_tokens=100,do_sample=False)
+
+parser = StrOutputParser()
+
+model1 = ChatHuggingFace(llm=llm)
+
+model2 = ChatGoogleGenerativeAI(model='gemini-3.6-flash')
+
+prompt = PromptTemplate(
+	template='Write a joke about {topic}',
+	input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+	template='Explain the following joke - {text}',
+	input_variables=['text']
+)
+
+chain = RunnableSequence(prompt, model2, parser, prompt2, model2, parser)
+
+res = chain.invoke({'topic':'Job'})
+
+print(res)
