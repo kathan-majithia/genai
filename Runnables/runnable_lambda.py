d8455042e2a1927da6d55af57b277b753a08faaf
Kathan <kathanmajithia@gmail.com>
2026-08-09 22:27:57 +0530

    Lambda runnables

---
+++ Runnables/runnable_lambda.py
@@ -0,0 +1,42 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_classic.output_parsers import StructuredOutputParser, ResponseSchema, PydanticOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from langchain_classic.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda
+
+from pydantic import BaseModel, Field
+from typing import Literal
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="Qwen/Qwen2.5-7B-Instruct",task="text-generation",max_new_tokens=100,do_sample=False)
+
+parser = StrOutputParser()
+
+model1 = ChatHuggingFace(llm=llm)
+
+model2 = ChatGoogleGenerativeAI(model='gemini-3.6-flash')
+
+prompt = PromptTemplate(
+	template='Write a joke about {topic}',
+	input_variables=['topic']
+)
+
+joke_gen_chain = RunnableSequence(prompt, model2, parser)
+
+def word_count(text):
+	return len(text.split())
+
+par_chain = RunnableParallel({
+	'joke': RunnablePassthrough(),
+	'word_count': RunnableLambda(lambda x:len(x.split()))
+	# 'word_count': RunnableLambda(word_count)
+})
+
+final_chain = RunnableSequence(joke_gen_chain,par_chain)
+
+res = final_chain.invoke('Cupcakes')
+
+print(res)
